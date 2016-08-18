6a5fde0abb16eb82998fd6c00b517df2ad482ae3
duoMe <duome@guila.la>
2016-08-18 15:42:24 +0200

    example, hello: use 0.8.0

--- examples/hello/blob/world/world.js
+++ examples/hello/blob/world/world.js
@@ -1,11 +1,11 @@
 this.view = function () {
    var self = this;
    return {
-   str: "world",
-   css: "world",
-   elm: {
-      svg: "ic_xilo_24px"
-   }
+      str: "world",
+      css: "world",
+      elm: {
+         svg: "ic_xilo_24px"
+      }
    };
 };
 
